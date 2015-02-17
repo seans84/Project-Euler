136b67f9f7082775067bdeed3b0e9cd2d024250d
Sean Szurko <sean_szurko@yahoo.com>
2015-02-16 23:19:05 -0800

    finished project 3

---
+++ project3.js
@@ -0,0 +1,18 @@
+var i = 600851475143
+function largestPrimeF(number) {
+	var divisor = 2;
+	var initialNum = number;
+	
+	while (initialNum > divisor) {
+		if (initialNum % divisor === 0) {
+			initialNum = initialNum / divisor;
+			divisor = 2;
+		}
+		else {
+			divisor++;
+		}
+	}
+	console.log("The largest prime factor of " + i + " is " + divisor)
+}
+
+largestPrimeF(i)
